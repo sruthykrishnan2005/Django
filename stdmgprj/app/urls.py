2983d388848e2511d2b909c45df0377f9de2ceaa
sruthykrishnan2005 <sruthykrishnan515@gmail.com>
2024-11-05 11:32:21 +0530

    stdadded

--- stdmgprj/app/urls.py
+++ stdmgprj/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from.import views
+from . import views
 
 urlpatterns=[
-    path('home',views.home)
+    path('home',views.home),
+    path('edit/<a>',views.edit_std),
+    path('delete/<a>',views.delete),
 ]
--- stdmgprj/app/views.py
+++ stdmgprj/app/views.py
@@ -14,3 +14,27 @@ def home(req):
        return redirect(home)
     else:
         return render(req,'home.html',{'students':std})
+    
+
+def edit_std(req,a):
+    data=''
+    for i in std:
+        if i['roll_no']==a:
+            data=i
+    if req.method=='POST':
+        roll=req.POST['roll_no']
+        name=req.POST['name']
+        age=req.POST['age']
+        data['roll_no']=roll
+        data['name']=name
+        data['age']=age
+        return redirect(home)
+    else:
+        return render(req,'edit.html',{'data':data})
+    
+
+def delete(req,a):
+    for i in std:
+        if i['roll_no']==a:
+            std.remove(i)
+    return redirect(home)
